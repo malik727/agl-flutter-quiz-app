d992c1c1ff055064b3a30ed71a1da7ac20f9003a
Malik Talha <talhamalik727x@gmail.com>
2023-04-04 21:55:53 +0500

    Build: initial files

---
+++ android/app/src/main/kotlin/com/example/agl_flutter_quiz_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.agl_flutter_quiz_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'AGL Quiz App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const <Widget>[
+            Text(
+              'My name is Malik Talha. I am excited to work on AGL as part of GSoC 2023',
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
